10524776ce4a3debdf972c89ed3cda73524852fa
JoshuaE <joshua.elsmore@digital.cabinet-office.gov.uk>
2026-03-09 13:13:07 +0000

    feat(LGDI-15): Change to allow dev deployment

--- scripts/developer-sandbox-setup.ts
+++ scripts/developer-sandbox-setup.ts
@@ -1,11 +1,24 @@
 // This script is to be executed via npm run development:sandbox:setup
-// Generates environment ID based on email signing the commits (git config -- set user.email)
+// Generates environment ID based on email signing the commits (git config -- set user.email) by default (directly configurable options available below)
 // Saves generated config into teraform/notifications/terraform.tfvars
 // If AWS environment variables are present
 // - prompts the creation if tfstate bucket does not exist
 // - prompts terraform initiation
+//
+// Usage:
+//   Developer sandbox setup
+//     npm run development:sandbox:setup
+//
+//   Changing to your colleagues dev environment (pair coding, debugging etc.)
+//     AS_DEVELOPER={their_email} npm run development:sandbox:setup
+//
+//   Changing to environment (i.e. dev, staging etc)
+//     AS_ENVIRONMENT={dev} npm run development:sandbox:setup
+//
+//   Note: This generator should only be used for setting bucket configuration.
 import { CreateBucketCommand, ListBucketsCommand, PutBucketVersioningCommand, S3Client } from '@aws-sdk/client-s3';
 import { GetCallerIdentityCommand, STSClient } from '@aws-sdk/client-sts';
+import { confirm } from '@inquirer/prompts';
 import { $, file } from 'bun';
 import { createHash } from 'node:crypto';
 
@@ -18,89 +31,138 @@ const unwrap = async <Result>(promise: Promise<Result>): Promise<[Result, undefi
   }
 };
 
-(async function () {
-  const tfvars = `./terraform/terraform.tfvars`;
-  // Generate unique values based on github user email
-  const email = (await $`git config --get user.email`).text().trim();
+const getConfig = async () => {
+  const prefix = `gdslgdi`;
+  const suffix = `s3-tfstate`;
+
+  // Allow running `AS_ENVIRONMENT=dev npm run development:sandbox:setup` to repoint local TF to non sandbox setups
+  if (process.env.AS_ENVIRONMENT !== undefined) {
+    return {
+      label: `For ${process.env.AS_ENVIRONMENT}`,
+      env: process.env.AS_ENVIRONMENT,
+      bucket: `${prefix}-${process.env.AS_ENVIRONMENT}-${suffix}`,
+    };
+  }
+
+  const email = process.env.AS_DEVELOPER
+    ? process.env.AS_DEVELOPER
+    : (await $`git config --get user.email`).text().trim();
+
+  // Generate Developer TF State bucket name based of email address configured within their Git
   const hash = createHash('md5').update(email).digest('hex').substring(4, 8);
-  const id = `${email.split('.').shift()}-${hash}`;
-  const bucket = `govuk-once-waste-management-dev-${id}-tfstate`;
+  const env = `${email.split('.').shift()}-${hash}`;
 
-  // Save to file
-  file(tfvars).write(`# Auto-generated
-# For developer: ${email}
+  return {
+    label: `For developer: ${email}`,
+    env,
+    bucket: `${prefix}-${env}-${suffix}`,
+  };
+};
 
-bucket          = "${bucket}"
-key             = "state.tfstate"
-region          = "eu-west-2"
-env             = "${id}"`);
+void (async function () {
+  try {
+    const tfvars = `./terraform/terraform.tfvars`;
 
-  // Ensure AWS env vars are available
-  if (
-    process.env.AWS_ACCESS_KEY_ID == undefined ||
-    process.env.AWS_SECRET_ACCESS_KEY == undefined ||
-    process.env.AWS_REGION == undefined
-  ) {
-    return console.log(
-      `No AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY present in env vars, please use 'eval $(gds-cli aws {accountName} -e)'`
-    );
-  }
+    const { env, bucket, label } = await getConfig();
 
-  // Fetch current account id
-  const [stsClient, s3Client] = [new STSClient(), new S3Client()];
-  const [identityResult, identityError] = await unwrap(stsClient.send(new GetCallerIdentityCommand()));
-  if (identityResult == undefined) {
-    return console.error(`Failed to fetch account ID :${identityError.message}`);
-  }
-  // Only log last 4 digits of account id
-  const accountIdHashed = identityResult.Account?.substring(8).padStart(identityResult.Account.length, '*');
-
-  // Fetch buckets
-  const [listBucketsResult, listBucketsError] = await unwrap(s3Client.send(new ListBucketsCommand()));
-  if (listBucketsResult == undefined) {
-    return console.error(`Failed to fetch buckets: ${listBucketsError.message}`);
-  }
-
-  // Prompt bucket creation if the state storage doesnt exist
-  if (listBucketsResult.Buckets?.map((bucket) => bucket.Name)?.includes(bucket) == false) {
-    // Prompt to create a bucket outlining AWS account used
-    if (confirm(`Would you like to create ${bucket} in ${accountIdHashed}`)) {
-      const [createBucketResult, createBucketError] = await unwrap(
-        s3Client.send(new CreateBucketCommand({ Bucket: bucket, ACL: 'private' }))
+    // Ensure AWS env vars are available
+    if (
+      process.env.AWS_ACCESS_KEY_ID == undefined ||
+      process.env.AWS_SECRET_ACCESS_KEY == undefined ||
+      process.env.AWS_REGION == undefined
+    ) {
+      return console.log(
+        `No AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY present in env vars, please use 'eval $(gds-cli aws {accountName} -e)'`
       );
-      if (createBucketResult == undefined) {
-        return console.error(`Failed creation of a ${bucket}: ${createBucketError.message}`);
-      }
-      console.log(`Created ${bucket} in ${createBucketResult.Location} ARN: ${createBucketResult.BucketArn}`);
-
-      // Enable versioning on the bucket
-      const [putBucketVersioningResult, putBucketVersioningError] = await unwrap(
-        s3Client.send(
-          new PutBucketVersioningCommand({
-            Bucket: bucket,
-            VersioningConfiguration: {
-              Status: 'Enabled',
-            },
-          })
-        )
-      );
-
-      if (putBucketVersioningResult == undefined) {
-        return console.error(`Failed enabling of versioning on ${bucket}: ${putBucketVersioningError?.message}`);
-      }
-      console.log(`Enabled versioning on the bucket`);
     }
-  } else {
-    console.log(`Developer tfstate bucket ${bucket} already exists in current ${accountIdHashed}`);
-  }
 
-  // Prompt to perform tf init
-  if (confirm(`Would you like to initialize terraform?`)) {
-    (
-      await $.cwd('terraform')`terraform init \
+    // Fetch current account id
+    const [stsClient, s3Client] = [new STSClient(), new S3Client()];
+    const [identityResult, identityError] = await unwrap(stsClient.send(new GetCallerIdentityCommand()));
+    if (identityResult == undefined) {
+      return console.error(`Failed to fetch account ID :${identityError.message}`);
+    }
+
+    // Only log last 4 digits of account id
+    const id = identityResult.Account ?? '';
+    const accountIdHashed = id.substring(8).padStart(id.length, '*');
+
+    // Detect if the env variables point outside of development account
+    if (accountIdHashed.endsWith('7518') == false) {
+      if (
+        (await confirm({
+          message: `AWS Account ${accountIdHashed} does not end with 7518 (development account), are you sure you want to continue?`,
+        })) == false
+      ) {
+        return console.log(`Exiting`);
+      }
+    }
+
+    // Fetch buckets
+    const [listBucketsResult, listBucketsError] = await unwrap(s3Client.send(new ListBucketsCommand()));
+    if (listBucketsResult == undefined) {
+      return console.error(`Failed to fetch buckets: ${listBucketsError.message}`);
+    }
+
+    // Prompt bucket creation if the state storage doesnt exist
+    if (listBucketsResult.Buckets?.map((bucket) => bucket.Name)?.includes(bucket) == false) {
+      // Prompt to create a bucket outlining AWS account used
+      if (await confirm({ message: `Would you like to create ${bucket} in ${accountIdHashed}` })) {
+        const [createBucketResult, createBucketError] = await unwrap(
+          s3Client.send(new CreateBucketCommand({ Bucket: bucket, ACL: 'private' }))
+        );
+        if (createBucketResult == undefined) {
+          return console.error(`Failed creation of a ${bucket}: ${createBucketError.message}`);
+        }
+        console.log(`Created ${bucket} in ${createBucketResult.Location} ARN: ${createBucketResult.BucketArn}`);
+
+        // Enable versioning on the bucket
+        const [putBucketVersioningResult, putBucketVersioningError] = await unwrap(
+          s3Client.send(
+            new PutBucketVersioningCommand({
+              Bucket: bucket,
+              VersioningConfiguration: {
+                Status: 'Enabled',
+              },
+            })
+          )
+        );
+
+        if (putBucketVersioningResult == undefined) {
+          return console.error(`Failed enabling of versioning on ${bucket}: ${putBucketVersioningError.message}`);
+        }
+        console.log(`Enabled versioning on the bucket`);
+      }
+    } else {
+      console.log(`Developer tfstate bucket ${bucket} already exists in current ${accountIdHashed}`);
+    }
+
+    // Save config to tfvars
+    void file(tfvars).write(`# Auto-generated by ${import.meta.file}
+# ${label}
+
+bucket   = "${bucket}"
+key      = "state.tfstate"
+region   = "eu-west-2"
+env      = "${env}"`);
+
+    // Prompt to perform tf init
+    if (await confirm({ message: `Would you like to initialize terraform?`, default: false })) {
+      (
+        await $.cwd('terraform')`terraform init\
       -backend-config "bucket=${bucket}" \
       -backend-config "key=state.tfstate" \
       -backend-config "region=eu-west-2"`
-    ).text();
+      ).text();
+    }
+
+  } catch (e) {
+    // Gracefully handle command+c exits
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (e?.name == 'ExitPromptError') {
+      console.log('\nCommand+c pressed, exiting...');
+      return;
+    }
+    throw e;
   }
 })();
